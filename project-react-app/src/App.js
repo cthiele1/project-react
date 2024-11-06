27d9d1d796ca0fab7d8ff5a5a1cc66cfcddc38fe
cthiele1 <cthiele@email.sc.edu>
2024-11-06 15:16:31 -0500

    render

--- project-react-app/src/App.js
+++ project-react-app/src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path="home" element={<Home />} />
         <Route path="about" element={<About />} />
