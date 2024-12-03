b59a50241bdd1d41dd025b02f4bd37307d2f2739
cthiele1 <cthiele@email.sc.edu>
2024-12-02 21:22:57 -0500

    changes

--- src/components/CookPlan.js
+++ src/components/CookPlan.js
@@ -1,10 +1,11 @@
 import "../css/CookPlan.css";
 import React, { useState } from "react";
+import DeleteCookPlan from "./DeleteCookPlan";
 
 const HousePlan = (props) => {
   const [showEditDialog, setShowEditDialog] = useState(false);
   const [showDeleteDialog, setShowDeleteDialog] = useState(false);
-  const [housePlan, setCookPlan] = useState(props);
+  const [CookPlan, setCookPlan] = useState(props);
   const [showCookPlan, setShowCookPlan] = useState(true);
 
   const openEditDialog = () => {
@@ -23,6 +24,14 @@ const HousePlan = (props) => {
     setShowDeleteDialog(false);
   };
 
+  const hideCookPlan = () => {
+    setCookPlan(false);
+  };
+
+  const editCookPlan = (newCookPlan) => {
+    setShowCookPlan(newCookPlan);
+  };
+
   return (
     <>
       {showCookPlan ? (
@@ -30,8 +39,9 @@ const HousePlan = (props) => {
           {showDeleteDialog ? (
             <DeleteCookPlan
               closeDialog={closeDeleteDialog}
-              name={housePlan.name}
-              _id={housePlan._id}
+              hideCookPlan={hideCookPlan}
+              name={CookPlan.name}
+              _id={CookPlan._id}
             />
           ) : (
             ""
@@ -40,39 +50,28 @@ const HousePlan = (props) => {
           {showEditDialog ? (
             <EditCookPlan
               closeDialog={closeEditDialog}
-              _id={housePlan._id}
-              name={housePlan.name}
-              size={housePlan.size}
+              updateCook={editCookPlan}
+              _id={CookPlan._id}
+              name={CookPlan.name}
+              size={CookPlan.size}
             />
           ) : (
             ""
           )}
 
-          <section className="cook-plan columns">
-            <section className="feature-image">
-              <img
-                src={"http://localhost:3001/" + housePlan.img_name}
-                alt={housePlan.name}
-              />
+          <section className="info">
+            <section id="change-buttons">
+              <a href="#" onClick={openEditDialog}>
+                &#9998;
+              </a>
+              <a href="#" onClick={openDeleteDialog}>
+                &#x2715;
+              </a>
             </section>
-            <section className="info">
-              <header className="columns">
-                <h3>{housePlan.name}</h3>
-                <section id="change-buttons">
-                  <a href="#" onClick={openEditDialog}>
-                    &#9998;
-                  </a>
-                  <a href="#" onClick={openDeleteDialog}>
-                    &#x2715;
-                  </a>
-                </section>
-              </header>
-
-              <p>Name: {housePlan.name}</p>
-              <p>Hometown: {housePlan.hometown}</p>
-              <p>Favorite Recipe: {housePlan.favorte_recipe}</p>
-              <p>Rating: {housePlan.rating}</p>
-            </section>
+            <p>Name: {props.name}</p>
+            <p>Hometown: {props.hometown}</p>
+            <p>Favorite Recipe: {props.favorte_recipe}</p>
+            <p>Rating: {props.rating}</p>
           </section>
         </div>
       ) : (
---
+++ src/components/DeleteCookPlan.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const DeleteCookPlan = (props) => {
+  const [result, setResult] = useState("");
+
+  const deleteCookPlan = async () => {
+    const response = await fetch(
+      `https://inclass-backend.onrender.com/api/house_plans/${props._id}`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    if (response.status === 200) {
+      setResult("Cook Succesfully Deleted");
+      props.hideCookPlan();
+      props.closeDialog();
+    } else {
+      setResult("Sorry, we couldn't delete your Cook at this time.");
+    }
+  };
+
+  return (
+    <div id="delete-dialog" className="w3-modal">
+      <div className="w3-modal-content">
+        <div className="w3-container">
+          <span
+            id="dialog-close"
+            className="w3-button w3-display-topright"
+            onClick={props.closeDialog}
+          >
+            &times;
+          </span>
+          <div id="delete-content">
+            <h3>Are you sure you want to delete the {props.name}</h3>
+            <section>
+              <button onClick={props.closeDialog}>No</button>
+              <button onClick={deleteCookPlan}>Yes</button>
+            </section>
+            <span>{result}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteCookPlan;
