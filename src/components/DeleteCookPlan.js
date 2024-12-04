d174d2b380c7bc0418230b0d96763b6261a1ed8d
cthiele1 <cthiele@email.sc.edu>
2024-12-03 21:17:22 -0500

    changes

--- src/components/DeleteCookPlan.js
+++ src/components/DeleteCookPlan.js
@@ -7,7 +7,7 @@ const DeleteCookPlan = (props) => {
     try {
       console.log(props);
       const response = await fetch(
-        `http://localhost:3001/api/house_plans/${props._id}`,
+        `https://inclass-backend.onrender.com/api/house_plans/${props._id}`,
         {
           method: "DELETE",
         }
